450acd8b6b1599b5cc7fc5dee8e853e614e1f608
rstyczynski <rstyczynski@gmail.com>
2020-04-20 22:42:13 +0200

    vcn nmap scanner

---
+++ bin/nmap_oci_vcn_notify.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+nmap_root=$1
+topic_id=$2
+
+function usage() {
+    echo "Usage: nmap_root topic_id"
+}
+
+[ -z "$nmap_root" ] && echo "Error. $(usage)" && exit 1
+[ -z "$topic_id" ] && echo "Error. $(usage)" && exit 1
+
+# check OCI commiunication
+timeout 30 oci os ns get
+if [ $? -ne 0 ]; then
+    echo "Error! No access to OCI API."
+    exit 1
+fi
+
+if [ ! -d $nmap_root/reports ]; then
+    echo "Error. Reports directory does not exist."
+    exit 1
+fi
+
+cd $nmap_root/reports
+
+if [ ! -d .git ]; then
+    echo "Error. Reports directory not initialized."
+    exit 1
+fi
+
+tmp=/tmp/$$
+mkdir -p $tmp
+
+declare -A reports_diff
+declare -A reports_diff_cnt
+
+for report_name in $(ls *.nmap); do
+    # 10 lines of change is ok due to DATE change, more means that file was really modified
+    reports_diff[$report_name]="$(git diff HEAD^^ HEAD $report_name)"
+    reports_diff_cnt[$report_name]=$(( $(git diff HEAD^^ HEAD $report_name | wc -l) - 10 ))
+done
+
+for report_name in ${!reports_diff_cnt[@]}; do
+    if [ ${reports_diff_cnt[$report_name]} -gt 0 ]; then
+        echo "Detected change in subnet: $report_name."
+
+        alert_title="Detected change in subnet: $report_name."
+        alert_body="${reports_diff[$report_name]}"
+        timeout 30 oci ons message publish --topic-id $topic_id --body "$alert_body" --title "$alert_title"
+    fi 
+done
+
---
+++ bin/nmap_oci_vcn_scan.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+nmap_root=$1
+compartment_id=$2
+vcn_cidr=$3
+scan_type=$4
+
+function usage() {
+    echo "Usage: nmap_root compartment_id vcn_cidr ssh|port|full"
+}
+
+[ -z "$nmap_root" ] && echo "Error. $(usage)" && exit 1
+[ -z "$compartment_id" ] && echo "Error. $(usage)" && exit 1
+[ -z "$vcn_cidr" ] && echo "Error. $(usage)" && exit 1
+[ -z "$vcn_cidr" ] && echo "Error. $(usage)" && exit 1
+
+tmp=/tmp/$$
+mkdir -p $tmp
+
+if [ ! -d $nmap_root/reports ]; then
+    echo "Error. Reports directory does not exist."
+    exit 1
+fi
+
+if [ ! -d $nmap_root/reports/.git ]; then
+    echo "Initializing reports directory."
+    $nmap_root/reports
+    git init
+    cd - >/dev/null
+fi
+
+# check OCI commiunication
+timeout 30 oci os ns get
+if [ $? -ne 0 ]; then
+    echo "Error! No access to OCI API."
+    exit 1
+fi
+
+# get vcn id
+oci network vcn list \
+    --compartment-id $compartment_id |
+    sed 's/-/XXX_XXX/g' |
+    jq -r ".data[] | select(.cidrXXX_XXXblock==\"$vcn_cidr\") | .id" |
+    sed 's/XXX_XXX/-/g' |
+    tee $tmp/vcn_id
+
+# get list of subnets
+vcn_id=$(cat $tmp/vcn_id)
+oci network subnet list --compartment-id $compartment_id --vcn-id $vcn_id --all |
+    sed 's/-/XXX_XXX/g' |
+    jq -r .data[].cidrXXX_XXXblock |
+    sed 's/XXX_XXX/-/g' |
+    tee $tmp/subnet_list
+
+# get reports
+for subnet in $(cat $tmp/subnet_list); do
+
+    subnet_report="$(echo $subnet | tr / .)_$scan_type.nmap"
+    case $scan_type in
+    ssh)
+        docker run -ti -w /root kali/nmap nmap --script nmap-vulners -sV -p22 $subnet |
+            # clean report
+            sed "s/[0-9][0-9]*\.[0-9][0-9]*s /NN.NNs /g" |
+            sed "s/[0-9][0-9]*\.[0-9][0-9]* latency /NN latency /g" |
+            sed "s/[0-9][0-9]*\.[0-9][0-9]* seconds/NN seconds/g" |
+            sed 's/Stats: [0-9][0-9]*:[0-9][0-9]*:[0-9][0-9]* elapsed; [0-9][0-9]* hosts completed ([0-9][0-9]* up)/Stats: HH:MM:SS elapsed; N hosts completed (0 up)/g' |
+            grep -v "Stats:" |
+            grep -v "Ping Scan Timing:" |
+            cat >$nmap_root/reports/$subnet_report
+        ;;
+    port)
+        docker run -ti -w /root kali/nmap nmap $subnet |
+            # clean report
+            sed "s/[0-9][0-9]*\.[0-9][0-9]*s /NN.NNs /g" |
+            sed "s/[0-9][0-9]*\.[0-9][0-9]* latency /NN latency /g" |
+            sed "s/[0-9][0-9]*\.[0-9][0-9]* seconds/NN seconds/g" |
+            sed 's/Stats: [0-9][0-9]*:[0-9][0-9]*:[0-9][0-9]* elapsed; [0-9][0-9]* hosts completed ([0-9][0-9]* up)/Stats: HH:MM:SS elapsed; N hosts completed (0 up)/g' |
+            grep -v "Stats:" |
+            grep -v "Ping Scan Timing:" |
+            cat cat >$nmap_root/reports/$subnet_report
+        ;;
+
+    full)
+        docker run -ti -w /root kali/nmap nmap --script nmap-vulners,vulscan --script-args vulscandb=scipvuldb.csv -sV $subnet |
+            # clean report
+            sed "s/[0-9][0-9]*\.[0-9][0-9]*s /NN.NNs /g" |
+            sed "s/[0-9][0-9]*\.[0-9][0-9]* latency /NN latency /g" |
+            sed "s/[0-9][0-9]*\.[0-9][0-9]* seconds/NN seconds/g" |
+            sed 's/Stats: [0-9][0-9]*:[0-9][0-9]*:[0-9][0-9]* elapsed; [0-9][0-9]* hosts completed ([0-9][0-9]* up)/Stats: HH:MM:SS elapsed; N hosts completed (0 up)/g' |
+            grep -v "Stats:" |
+            grep -v "Ping Scan Timing:" |
+            cat cat >$nmap_root/reports/$subnet_report
+        ;;
+    esac
+
+    (
+        cd $nmap_root/reports
+        git add *
+        git commit -m "nmap scan  type $scan_type"
+        cd - >/dev/null
+    )
+done
+
+rm -rf /tmp/$$
