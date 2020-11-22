d5a3c75918bd861b03ba87352bfa328f9f873d00
rstyczynski <rstyczynski@gmail.com>
2020-11-22 09:47:45 +0100

    .

--- bin/enable_jfr_collection.sh
+++ bin/enable_jfr_collection.sh
@@ -19,9 +19,9 @@ function make_jfr_cron() {
     cron_section_start="# START jfr - $wls_server@$domain_name"
     cron_section_stop="# STOP jfr - $wls_server@$domain_name"
 
-    cat >jfr.cron <<EOF
+   cat >jfr.cron <<EOF
 $cron_section_start
-*/15 * * * * ~/wls-tools/bin/wls_jfr.sh $wls_server start 890s dump_location $jfr_location
+*/15 * * * * ~/wls-tools/bin/wls_jfr.sh $wls_server stop; sleep 1; ~/wls-tools/bin/wls_jfr.sh $wls_server start 900 dump_location $jfr_location
 $cron_section_stop
 EOF
 
