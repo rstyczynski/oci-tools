d4bc01fcc990741486e3d639366dc0d12d705406
rstyczynski <rstyczynski@gmail.com>
2020-11-22 09:13:30 +0100

    .

--- bin/enable_jfr_collection.sh
+++ bin/enable_jfr_collection.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function getmwlogs() {
-    env_files=$(cat /etc/x-ray.config | tail -1 2>/dev/null | grep env_files | cut -f2 -d=)
+    env_files=$(cat /etc/x-ray.config 2>/dev/null | grep env_files | tail -1 | cut -f2 -d=)
     test -z "$env_files" && read -p "Enter location of mwlogs (defaults to /mwlogs):" env_files
     test -z "$env_files" && env_files=/mwlogs
 
