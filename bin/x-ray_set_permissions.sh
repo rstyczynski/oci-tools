44fe58955363e656f4e6f6fff23231bc78b229d3
rstyczynski <rstyczynski@gmail.com>
2020-11-23 21:58:34 +0100

    set permissions

---
+++ bin/x-ray_set_permissions.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source ~/oci-tools/bin/config.sh
+
+
+export env_files=$(getcfg x-ray env_files | tr [A-Z] [a-z])
+export env=$(getcfg x-ray env | tr [A-Z] [a-z])
+export component=$(getcfg x-ray component | tr [A-Z] [a-z])
+
+sudo mkdir -p $env_files/x-ray/$env 
+sudo mkdir -p $env_files/x-ray/$env/$component/diag/hosts
+sudo mkdir -p $env_files/x-ray/$env/$component/diag/wls/log
+sudo mkdir -p $env_files/x-ray/$env/$component/watch/hosts
+
+sudo chmod 777 $env_files/x-ray
+sudo chmod 777 $env_files/x-ray/$env
+sudo chmod 777 $env_files/backup
+sudo chmod 777 $env_files/x-ray/$env/$component/diag/hosts
+sudo chmod 777 $env_files/x-ray/$env/$component/diag/wls/log 
+sudo chmod 777 $env_files/x-ray/$env/$component/watch/hosts
+
