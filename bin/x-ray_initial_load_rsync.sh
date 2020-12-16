e4cbe5527a834dde60679959ceca3fcaba994e52
Ryszard Styczynski <ryszard.styczynski@oracle.com>
2020-12-16 10:10:38 +0100

    initial load modes added

--- bin/x-ray_initial_load_rsync.sh
+++ bin/x-ray_initial_load_rsync.sh
@@ -28,19 +28,33 @@ function initial_load_rsync() {
         echo "Processing diagnostics source: $diagname/$log"
         echo "##########################################"
 
+        mode=$(cat $diag_cfg | y2j | jq -r ".diagnose.$log.mode" | rn)
+        : ${mode:=flat2date}
+
         src_dir=$(cat $diag_cfg | y2j | jq -r ".diagnose.$log.dir" | rn)
         expose_dir=$(cat $diag_cfg | y2j | jq -r ".diagnose.$log.expose.dir" | rn)
 
-        initial_rsync "$src_dir" "$expose_dir" "$diagname\_$log"
+        case $mode in
+        flat2date)
+            initial_rsync_flat2date "$src_dir" "$expose_dir" "$diagname\_$log"
+            ;;
+        date2date)
+            initial_rsync_date2date "$src_dir" "$expose_dir" "$diagname\_$log"
+            ;;
+        *)
+            echo "Error. Sync mode unknown: $mode. Cannot continue"
+            exit 1
+            ;;
+        esac
     done
 }
 
-function initial_rsync() {
+function initial_rsync_flat2date() {
     src_dir=$1
     expose_dir=$2
     name=$3
 
-    : ${name:=initial_rsync}
+    : ${name:=initial_rsync_flat2date}
     iload_tmp=~/tmp/initialload.$name
     rm -rf $iload_tmp
     mkdir -p $iload_tmp
@@ -48,7 +62,7 @@ function initial_rsync() {
     todayiso8601="\$(date -I)"
 
     echo "##########################################"
-    echo "Processing initial sync: $src_dir $expose_dir $name"
+    echo "Processing initial sync in flat2date modes: $src_dir $expose_dir $name"
     echo "##########################################"
 
     # in fact: replace $(date -I) into current date.
@@ -69,7 +83,7 @@ function initial_rsync() {
         # in fact: replace $(date -I) into current date.
         dst_dir=$(echo "$expose_dir" | sed "s/$todayiso8601/$date/g" | sed "s/\$(hostname)/$(hostname)/g")
 
-        # rsync creates dst dir, but crete to have right permissions
+        # rsync creates dst dir, but create to have right permissions
         mkdir -p $dst_dir
         chmod g+rx $dst_dir
         chmod o+rx $dst_dir
@@ -89,5 +103,36 @@ function initial_rsync() {
     rm -rf $iload_tmp
  }
 
+
+function initial_rsync_date2date() {
+    src_dir=$1
+    expose_dir=$2
+    name=$3
+
+    : ${name:=initial_rsync_date2date}
+    iload_tmp=~/tmp/initialload.$name
+    rm -rf $iload_tmp
+    mkdir -p $iload_tmp
+
+    todayiso8601="\$(date -I)"
+
+    echo "##########################################"
+    echo "Processing initial sync in date2date modes: $src_dir $expose_dir $name"
+    echo "##########################################"
+
+    # in fact: remove $(date -I) from end of path
+    src_dir=$(echo "$src_dir" | sed "s/$todayiso8601$//g" | sed "s/\$(hostname)/$(hostname)/g")
+    dst_dir=$(echo "$expose_dir" | sed "s/$todayiso8601$//g" | sed "s/\$(hostname)/$(hostname)/g")
+
+    # chmod does not work properly on some rsync e.g. 3.0.6; added  umask to fix        
+    umask 022
+    #rsync  --dry-run \
+    rsync --progress -h \
+    -t \
+    --chmod=Fu=r,Fgo=r,Dgo=rx,Du=rwx \
+    $src_dir $dst_dir
+
+    rm -rf $iload_tmp
+ }
 
 initial_load_rsync $@
