5e700c8e6c8c912bc3aa9fce8b5e99c48096ac5c
Ryszard Styczynski <ryszard.styczynski@oracle.com>
2021-03-23 10:15:17 +0100

    .

--- bin/set_environment.sh
+++ bin/set_environment.sh
@@ -2,14 +2,17 @@
 
 if [ -z "$env_files" ]; then
   echo "Error. Environment shared root dir not configured. Exiting."
-  exit 1
+  return 1
 fi
 
 source $env_files/tools/oci-tools/bin/config.sh
 export os_user=$(getcfg x-ray mw_owner)
 
 # ohs only? take data from node manager.
-: ${os_user:=$(ps aux | grep weblogic.nodemanager | grep -v grep | cut -f1 -d' ' | sort -u)}
+if [ -z "$os_user" ]; then
+  os_user:=$(ps aux | grep weblogic.nodemanager | grep -v grep | cut -f1 -d' ' | sort -u)
+  setcfg x-ray mw_owner ${os_user:=undefined} force
+fi
 
 if [ -z "$os_user" ]; then
   source $env_files/tools/wls-tools/bin/discover_processes.sh
@@ -17,14 +20,13 @@ if [ -z "$os_user" ]; then
   os_user=$(getWLSjvmAttr ${wls_managed[0]} os_user)
   # admin only?
   : ${os_user:=$(getWLSjvmAttr ${wls_admin[0]} os_user)}
+
+  setcfg x-ray mw_owner ${os_user:=undefined} force
 fi
 
 if [ -z "$os_user" ]; then
   echo 'Error. MW owner user not found'
-  exit 1
-else
-  setcfg x-ray mw_owner ${os_user:=undefined} force
-
+  return 1
 fi
 
 source $env_files/tools/oci-tools/bin/config.sh
@@ -34,7 +36,7 @@ export component=$(getcfg x-ray component)
 
 if [ -z "$env" ] || [ -z "$component" ]; then
   echo "Error. Environment not configured. Exiting."
-  exit 1
+  return 1
 fi
 
 cat <<EOF
