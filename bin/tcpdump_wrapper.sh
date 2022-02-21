bf9d09de18c66028015f5cd4d9753e95648fc62b
Ryszard Styczynski <ryszard.styczynski@oracle.com>
2022-02-21 07:05:32 +0100

    stop capture fix

--- bin/tcpdump_wrapper.sh
+++ bin/tcpdump_wrapper.sh
@@ -13,18 +13,17 @@ function tcpdump_start() {
             echo "Started. Use dump|tail to check traffic. Use stop to finish capture."
         else
             echo "Already running"
-            ps aux | grep tcpdump | grep ${pcap_dir} | grep ${tcp_file_pfx} 
+            ps aux | grep tcpdump | grep ${tcp_file_pfx} 
         fi
 }
 
 function tcpdump_stop() {
-        if [ $(ps aux | grep tcpdump | grep ${pcap_dir} | grep ${tcp_file_pfx} | grep -v grep | wc -l) -eq 0 ]; then
+        if [ $(ps aux | grep tcpdump | grep ${tcp_file_pfx} | grep -v grep | wc -l) -eq 0 ]; then
             echo "Capture not running."
         else
             echo -n "Stopping tdpdump at $netif of traffic to ${pcap_filter}..."
-            sudo kill $(ps aux | grep tcpdump | grep ${pcap_dir} | grep ${tcp_file_pfx} | grep -v grep | tr -s ' ' | cut -d' ' -f2)
-            tcp_file=$(ls -t ${pcap_dir}/*/${tcp_file_pfx}_* | head -1)
-            echo "Done. Capture file: $tcp_file"
+            sudo kill $(ps aux | grep tcpdump | grep ${tcp_file_pfx} | grep -v grep | tr -s ' ' | cut -d' ' -f2)
+            echo "Done. Capture files in : ${pcap_dir}"
         fi
 }
 
@@ -38,37 +37,45 @@ function tcpdump_wrapper() {
     : ${pcap_dir:=$HOME/x-ray/net/traffic/$(date -I)} 
     : ${netif:=$(ip a | grep -i mtu | grep -v lo: | head -1 | tr -d ' ' | cut -f2 -d:)}
 
-    tcp_file_pfx="tcpdump_filter_$(echo ${pcap_filter} | tr -c 'a-zA-Z0-9' '_')"
+    tcp_file_pfx=tcpdump_filter_$(echo ${pcap_filter} | tr -c 'a-zA-Z0-9' '_')
 
     case $cmd in
     start)
-	tcpdump_start
+        tcpdump_start
         ;;
     stop)
-	tcpdump_stop
+        tcpdump_stop
         ;;
     restart)
-	tcpdump_stop
-	tcpdump_start
+        tcpdump_stop
+        tcpdump_start
         ;;
     status)
-        if [ $(ps aux | grep tcpdump | grep ${pcap_dir} | grep ${tcp_file_pfx} | grep -v grep | wc -l) -eq 0 ]; then
+        if [ $(ps aux | grep tcpdump | grep ${tcp_file_pfx} | grep -v grep | wc -l) -eq 0 ]; then
             echo "Capture not running."
         else
             echo "Capture running"
-            ps aux | grep tcpdump | grep ${pcap_dir} | grep ${tcp_file_pfx} | grep -v grep
+            ps aux | grep tcpdump | grep ${tcp_file_pfx} | grep -v grep
         fi
         ;;
     dump)
-	echo "Dump of files from ${pcap_dir}:"	
-    	for tcp_file in $(ls -t ${pcap_dir}/${tcp_file_pfx}_*); do
-           tcpdump -A -nn -r  $tcp_file
-	done
+        echo "Dump of files from ${pcap_dir}:"
+        if [ $(ls -t ${pcap_dir}/${tcp_file_pfx}_* 2>/dev/null | wc -l) -gt 0 ]; then
+           for tcp_file in $(ls -t ${pcap_dir}/${tcp_file_pfx}_* 2>/dev/null); do
+             tcpdump -A -nn -r  $tcp_file
+           done
+        else
+           echo None.
+        fi
         ;;
     tail)
-        tcp_file=$(ls -t ${pcap_dir}/${tcp_file_pfx}_* | head -1)
-        echo "Tail of $tcp_file:"
-        tcpdump -A -nn -r $tcp_file | tail 
+        if [ $(ls -t ${pcap_dir}/${tcp_file_pfx}_* 2>/dev/null | wc -l) -gt 0 ]; then
+           tcp_file=$(ls -t ${pcap_dir}/${tcp_file_pfx}_* | head -1)
+           echo "Tail of $tcp_file:"
+           tcpdump -A -nn -r $tcp_file | tail 
+        else
+           echo None.
+        fi
         ;;
     '')
         echo "Usage: tcpdump_host ifname filter start|stop|status|dump|tail"
