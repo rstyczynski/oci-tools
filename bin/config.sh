1a902ffcfed13ec13867a3602d8bce3a6150d429
Ryszard Styczynski <ryszard.styczynski@oracle.com>
2022-03-22 11:13:52 +0100

    .

--- bin/config.sh
+++ bin/config.sh
@@ -13,16 +13,18 @@ function getcfg() {
     value_row=$(cat /etc/$which.config 2>/dev/null | grep "^$what=" | tail -1 | grep "^$what=" )
     if [ $? -eq 0 ]; then
         if [ "$info" == show_file ]; then
-            echo "Config file: /etc/$which.config"
+            echo "$which/$what has value $(echo $value_row | cut -d= -f2) stored in config file: /etc/$which.config"
+        else
+            echo $value_row | cut -d= -f2
         fi
-        echo $value_row | cut -d= -f2
     else
             value_row=$(cat ~/.$which/config 2>/dev/null | grep "^$what=" | tail -1 | grep "^$what=" )
             if [ $? -eq 0 ]; then
                 if [ "$info" == show_file ]; then
-                    echo "Config file: ~/.$which/config "
+                    echo "$which/$what has value $(echo $value_row | cut -d= -f2) stored in config file: /etc/$which.config"
+                else
+                    echo $value_row | cut -d= -f2
                 fi
-                echo $value_row | cut -d= -f2
             else
                 return 1
             fi
