7358b6446557d833cdab2d881efd037404fec7ec
Ryszard Styczynski <ryszard.styczynski@oracle.com>
2022-04-08 17:19:02 +0200

    .

--- bin/oci_fetch_logs.sh
+++ bin/oci_fetch_logs.sh
@@ -49,7 +49,7 @@ test ! -z "$missing_tools" && named_exit "Required tools not available." "$missi
 # Parameters are reflected in shell varaibles which are set with parameter value. 
 # No value parameters are set to 'set' if exist in cmd line arguents
 
-valid_opts=$(getopt --longoptions $script_args,$script_args_persist,$script_args_system -- "$@")
+valid_opts=$(getopt --longoptions "$script_args,$script_args_persist,$script_args_system" -- "$@")
 eval set -- "$valid_opts"
 
 while [[ $# -gt 0 ]]; do
