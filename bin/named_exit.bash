e1fc1789af6e6a4e1b48da05fe6e8bb63f7f5672
Ryszard Styczynski <ryszard.styczynski@oracle.com>
2022-06-15 11:31:16 +0200

    # store labels and code in associative array

--- bin/named_exit.bash
+++ bin/named_exit.bash
@@ -18,7 +18,7 @@
 #
 
 lib_name='named_exit.bash'
-lib_version='1.0'
+lib_version='1.1'
 lib_by='ryszard.styczynski@oracle.com'
 
 lib_tools=''
@@ -28,28 +28,28 @@ lib_cfg=''
 # lib code
 #
 
-declare -A named_exit.exit_label
-declare -A named_exit.exit_code
+declare -A named_exit_exit_label
+declare -A named_exit_exit_code
 
 function set_exit_code_variable() {
-  desc=$1
-  code=$2
+  local desc=$1
+  local code=$2
 
   desc_id=$(echo "$desc" | sha256sum | cut -f1 -d' ')
-  named_exit.exit_label[desc_id]="$desc"
-  named_exit.exit_code[desc_id]=$code
+  named_exit_exit_label[desc_id]="$desc"
+  named_exit_exit_code[desc_id]=$code
 }
 
 function get_exit_code_variable() {
-  desc=$1
+  local desc=$1
 
   desc_id=$(echo "$desc" | sha256sum | cut -f1 -d' ')
-  echo ${named_exit.exit_code[desc_id]}
+  echo ${named_exit_exit_code[desc_id]}
 }
 
 function named_exit() {
-  desc=$1
-  info=$2
+  local desc=$1
+  local info=$2
 
   exit_code=$(get_exit_code_variable "$desc")
   if [ -z "$exit_code" ]; then
@@ -59,7 +59,7 @@ function named_exit() {
   else
     if [ -z "$info" ];then
       >&2 echo
-      >&2 echo $desc
+      >&2 echo "$desc"
     else
       >&2 echo
       >&2 echo "$desc Info: $info"
